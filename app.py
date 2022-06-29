f755bf1818e711dd796da21eddaeff031bf9b7ba
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-06-29 14:34:15 -0500

    create base User model and server for app

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, request, render_template, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
+# connect app to database
 connect_db(app)
-db.create_all()
+
+@app.route('/users')
+def list_users():
+    return render_template("users.html")
+
+@app.route('/')
+def base():
+    return redirect('/users')
--- models.py
+++ models.py
@@ -1,1 +1,18 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User database model"""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(500), default='./assets/sincerely-media-vcF5y2Edm6A-unsplash.jpg')
